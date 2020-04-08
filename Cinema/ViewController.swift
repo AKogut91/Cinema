cae359a48fbc594ade9af0597655d5fa3c89dc9f
AlexanderKogut <alexander.kogyt@gmail.com>
2020-04-08 17:49:00 +0300

    Initial Commit

---
+++ Cinema/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cinema
+//
+//  Created by AlexanderKogut on 4/8/20.
+//  Copyright © 2020 AlexanderKogut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
